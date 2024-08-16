82521187cd3c9845cb159b8da46c26b2137e78d2
qiqzhao <bme_ritter@foxmail.com>
2024-08-16 17:49:39 +0800

    feat: trade experiences

--- src/component/trading-experiences/index.tsx
+++ src/component/trading-experiences/index.tsx
@@ -1,4 +1,33 @@
+import MobileDevice from "../../assets/mobile-devices.png";
 import "./index.css";
+
 export const TradingExperience = () => {
-  return <section>TradingExperience</section>;
+  return (
+    <section className="trading-experience">
+      <div className="trading-experience-container">
+        <div className="trading-experience-content">
+          <h3>A World-Class Trading Experience</h3>
+          <p>
+            Experience the FP Markets difference with 18 years of expertise with
+            our award-winning service, top-tier trading platforms, 24/7
+            multi-lingual support, extensive market access - all combined to
+            enhance your trading journey.
+          </p>
+          <h3>Explore a World of Trading Options</h3>
+          <p>
+            Trade with over 70+ Forex pairs, over 10,000 stocks, major global
+            indices, diverse commodities, bonds, metals, and popular
+            cryptocurrencies tailored for every trading style.
+          </p>
+          <h3>Empower Trades with Cutting-Edge Platforms</h3>
+          <p>
+            Gain access to top-rated platforms and advanced tools for an edge in
+            the financial markets.
+          </p>
+        </div>
+
+        <img src={MobileDevice} alt="" />
+      </div>
+    </section>
+  );
 };
