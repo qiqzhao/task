a4d4f4b6fccc6aa2f6a3901a547f8397830dc056
qiqzhao <bme_ritter@foxmail.com>
2024-08-16 17:12:32 +0800

    feat: trade trust & awards counting

--- src/component/awards-counting/index.tsx
+++ src/component/awards-counting/index.tsx
@@ -1,4 +1,25 @@
 import "./index.css";
+import MostSatisfied from "../../assets/most-satisfied.svg";
+import BestEducationMaterial from "../../assets/best-education-material.svg";
+import BestGlobal from "../../assets/best-global.svg";
+import BestCustomerService from "../../assets/best-customer-service.svg";
+import BestTradeExcetion from "../../assets/best-trade-execution.svg";
+import BestCfoBroker from "../../assets/best-cfd-broker.svg";
+import CompareForexBroker from "../../assets/compare-forex-broker.svg";
+
 export const AwardsCounting = () => {
-  return <section>awards counting</section>;
+  return (
+    <section className="awards-counting">
+      <h2 className="awards-counting-title">Over 40 Global Industry Awards and Counting</h2>
+      <div className="awards-counting-award">
+        <img src={MostSatisfied} alt="" />
+        <img src={BestTradeExcetion} alt="" />
+        <img src={BestGlobal} alt="" />
+        <img src={BestEducationMaterial} alt="" />
+        <img src={BestCustomerService} alt="" />
+        <img src={BestCfoBroker} alt="" />
+        <img src={CompareForexBroker} alt="" />
+      </div>
+    </section>
+  );
 };
--- src/component/trade-trust/index.tsx
+++ src/component/trade-trust/index.tsx
@@ -1,4 +1,21 @@
 import "./index.css";
+import TrustPilot from "../../assets/trustpilot.svg";
+
 export const TradeTrust = () => {
-  return <section>TradeTrust</section>;
+  return (
+    <section className="trade-trust">
+      <h1 className="trade-trust-title">
+        Trade with <br /> a Trusted <br /> Global Broker
+      </h1>
+      <img src={TrustPilot} alt="trustpilot" />
+      <form className="trade-trust-form">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Email" />
+        <input type="number" placeholder="Phone" />
+        <button>Register Now</button>
+      </form>
+      <div className="trade-trust-overlay"></div>
+    </section>
+  );
 };
