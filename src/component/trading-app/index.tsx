7e1b56d31c000fb34e76bd996bcd90d222ee894b
qiqzhao <bme_ritter@foxmail.com>
2024-08-17 14:09:17 +0800

    feat: trading app

--- src/component/trading-app/index.tsx
+++ src/component/trading-app/index.tsx
@@ -1,4 +1,21 @@
 import "./index.css";
+
 export const TradingApp = () => {
-  return <section>trading app</section>;
+  return (
+    <section className="trade-app">
+      <div className="trade-app-bg">
+        <div className="trade-app-content">
+          <h3>FP Markets Trading App</h3>
+          <p>
+            Get low-cost trading on the go with the all-new FP Markets Trading
+            App.
+          </p>
+          <p>
+            Access Forex, Share CFDs, Indices, Commodities, and Cryptocurrencies
+            all from one convenient app through one account.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
