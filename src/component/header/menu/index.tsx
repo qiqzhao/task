4d201284b3852a0174e3c08794b71897d25d157d
qiqzhao <bme_ritter@foxmail.com>
2024-08-18 13:39:52 +0800

    feat: menu select & hovor style

--- src/component/header/menu/index.tsx
+++ src/component/header/menu/index.tsx
@@ -5,7 +5,7 @@ export const Menu = () => {
     <section className="menu">
       <div className="menu-item">Partners hub home</div>
       <div className="menu-item">Partnership programs</div>
-      <div className="menu-item">Tools and resources</div>
+      <div className="menu-item selected">Tools and resources</div>
       <div className="menu-item">FAQ’s</div>
       <div className="menu-item">Social trading</div>
       <div className="menu-item">Marketing advice</div>
